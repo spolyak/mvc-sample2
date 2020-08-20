3125ec48806aadf959bae954263f7787c4872bfe
spolyak <stevepolyak@mac.com>
2020-08-20 13:12:51 -0500

    Initial Commit

---
+++ mvc-sample2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvc-sample2
+//
+//  Created by Steve Polyak on 8/20/20.
+//  Copyright © 2020 Steve Polyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
